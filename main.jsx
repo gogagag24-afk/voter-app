15b99787742e0b4893a1d9806c4a4e6c0e40a74e
gogagag24-afk <gogagag24@gmail.com>
2026-04-25 23:55:10 +0400

    fix: add main.jsx entry

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import VoterApp from "./voter-app.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <VoterApp />
+  </React.StrictMode>
+);
